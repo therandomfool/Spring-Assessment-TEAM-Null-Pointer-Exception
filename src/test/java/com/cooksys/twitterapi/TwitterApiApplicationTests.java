99361026d697f46f96b3f5ba4db7147849b8bf69
Will Marttala <wmarttala123@gmail.com>
2021-03-15 16:35:40 -0500

    Initial Project Structure

---
+++ src/test/java/com/cooksys/twitterapi/TwitterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooksys.twitterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
