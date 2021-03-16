204e1d57a9922be3086fe916e136c5d59a1cde4d
Julio Corzo <juliocorzo@me.com>
2021-03-16 16:22:31 -0700

    Fixed Service implementation folder.

--- src/main/java/com/cooksys/twitterapi/services/implementations/UserServiceImpl.java
+++ src/main/java/com/cooksys/twitterapi/services/implementations/UserServiceImpl.java
@@ -1,5 +1,12 @@
 package com.cooksys.twitterapi.services.implementations;
 
-public class UserServiceImpl {
+import com.cooksys.twitterapi.mappers.UserMapper;
+import com.cooksys.twitterapi.repositories.UserRepository;
+import com.cooksys.twitterapi.services.UserService;
+
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+    private UserMapper userMapper;
     
 }
