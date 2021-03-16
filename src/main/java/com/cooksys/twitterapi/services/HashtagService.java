98432d0d71372da696f4afee2ab45e3052b26045
therandomfool <mddenver@protonmail.com>
2021-03-16 16:35:31 -0700

    changes what changes

--- src/main/java/com/cooksys/twitterapi/services/HashtagService.java
+++ src/main/java/com/cooksys/twitterapi/services/HashtagService.java
@@ -1,9 +1,5 @@
 package com.cooksys.twitterapi.services;
 
-import java.util.List;
-
-import com.cooksys.twitterapi.model.HashtagResponseDTO;
-
 public interface HashtagService {
 //	List<HashtagResponseDTO> getAllHashtags();
 
--- src/main/java/com/cooksys/twitterapi/services/UserService.java
+++ src/main/java/com/cooksys/twitterapi/services/UserService.java
@@ -1,6 +1,4 @@
 package com.cooksys.twitterapi.services;
-import java.util.List;
-import com.cooksys.twitterapi.model.*;
 
 public interface UserService{
 
