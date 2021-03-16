cd97eaf4ede56518ccb20b2b65c17815e1a7508a
Julio Corzo <juliocorzo@me.com>
2021-03-16 10:56:16 -0700

    Implemented ValidateService and modified HashtagService.

--- src/main/java/com/cooksys/twitterapi/services/HashtagService.java
+++ src/main/java/com/cooksys/twitterapi/services/HashtagService.java
@@ -2,7 +2,7 @@ package com.cooksys.twitterapi.services;
 
 import java.util.List;
 
-import com.cooksys.twitterapi.model.HashtagResponseDTO;
+import com.cooksys.twitterapi.model.HashtagResponseDto;
 
 public interface HashtagService {
 //	List<HashtagResponseDTO> getAllHashtags();
