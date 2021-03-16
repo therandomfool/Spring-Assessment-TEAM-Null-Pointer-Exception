b0c4dff68b048ee5fad14642e3686d034a0bbd99
Julio Corzo <juliocorzo@me.com>
2021-03-16 09:58:39 -0700

    Implemented CredentialRequestDto and CredentialResponseDto.

---
+++ src/main/java/com/cooksys/twitterapi/model/CredentialResponseDto.java
@@ -0,0 +1,14 @@
+package com.cooksys.twitterapi.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class CredentialResponseDto {
+
+    private String username;
+
+    private String password;
+
+}
